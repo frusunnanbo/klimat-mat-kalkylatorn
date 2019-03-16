47e9d81f287fd093836012abdccf554df1c97750
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-03-16 23:05:55 +0100

    Kalkylera koldioxid

--- src/app.elm
+++ src/app.elm
@@ -1,5 +1,7 @@
 module Main exposing (..)
 
+import Dict exposing (Dict)
+
 import Browser
 import Html exposing (Html, Attribute, h1, div, input, text, table, tr, td)
 import Html.Attributes exposing (..)
@@ -22,6 +24,7 @@ main =
 type alias Model =
     { content : String
     , foods : List Foods.Food
+    , amounts : Dict String Float
     }
 
 
@@ -29,6 +32,7 @@ init : Model
 init =
     { content = ""
     , foods = Foods.foods
+    , amounts = Dict.empty
    }
 
 
@@ -38,6 +42,7 @@ init =
 
 type Msg
     = Change String
+    | NewAmount String Float
 
 
 update : Msg -> Model -> Model
@@ -45,6 +50,8 @@ update msg model =
     case msg of
         Change newContent ->
             { model | content = newContent, foods = filterFoods newContent }
+        NewAmount foodName amount ->
+            { model | amounts = Dict.insert foodName amount model.amounts }
 
 filterFoods : String -> List Foods.Food
 filterFoods userInput =
@@ -65,21 +72,41 @@ view model =
         , div
         []
         [ input [ placeholder "Mata in mat", value model.content, onInput Change ] []
-        , div [] [ foods model.foods ]
+        , div [] [ foods model.foods model.amounts ]
+        , div [] [ text ("Summa: " ++ (String.fromFloat (sumCO2 model.amounts)) ++ " kg C02-ekvivalenter") ]
         ]
         ]
 
-foods : List Foods.Food -> Html Msg
-foods foodList =
-  table [] (List.map foodRow foodList)
+foods : List Foods.Food -> Dict String Float -> Html Msg
+foods foodList amounts =
+  table [] (List.map (foodRow amounts) foodList)
 
-foodRow : Foods.Food -> Html Msg
-foodRow  food =
+foodRow : Dict String Float -> Foods.Food -> Html Msg
+foodRow  amounts food =
   tr []
     [ td [] [ text food.name ]
+    , td [] [ input [ placeholder "kg mat", value (getAmount food.name amounts), onInput (newAmount food.name) ] [] ]
     , td [] [ text (formatCo2Equivivalents food.co2equivalents) ]
     ]
+
+getAmount : String -> Dict String Float -> String
+getAmount foodName amounts =
+  Dict.get foodName amounts
+  |> Maybe.map String.fromFloat
+  |> Maybe.withDefault ""
+
+newAmount : String -> String -> Msg
+newAmount foodName amount =
+  NewAmount foodName (Maybe.withDefault 0 (String.toFloat amount))
 
 formatCo2Equivivalents : Float -> String
 formatCo2Equivivalents co2equivalents =
   String.fromFloat co2equivalents ++ " kg CO2"
+
+sumCO2 : Dict String Float -> Float
+sumCO2 amounts =
+  Dict.foldl addFoodAmount 0 amounts
+
+addFoodAmount : String -> Float -> Float -> Float
+addFoodAmount foodName amount accumulated =
+  accumulated + (Foods.getC02equivalent foodName amount)
--- src/foods.elm
+++ src/foods.elm
@@ -1,9 +1,35 @@
-module Foods exposing (Food, foods)
+module Foods exposing (Food, foods, getC02equivalent)
+
+import Dict exposing (Dict)
 
 type alias Food =
   { name : String
   , co2equivalents : Float
   }
+
+foodsDict =
+  Dict.fromList
+  [ ("Nötkött", Food "Nötkött" 31.0)
+  , ( "Lammkött", Food "Lammkött" 27.0)
+  , ( "Ost", Food "Ost" 14.0)
+  , ( "Griskött", Food "Griskött" 7.0)
+  , ( "Fisk", Food "Fisk" 6.0)
+  , ( "Kyckling", Food "Kyckling" 4.0)
+  , ( "Ägg", Food "Ägg" 2.0)
+  , ( "Ris", Food "Ris" 2.0)
+  , ( "Mjölk", Food "Mjölk" 1.4)
+  , ( "Tomater", Food "Tomater" 1.5)
+  , ( "Baljväxter", Food "Baljväxter" 0.8)
+  , ( "Bananer", Food "Bananer" 0.9)
+  , ( "Apelsiner", Food "Apelsiner" 0.9)
+  , ( "Mjukt bröd", Food "Mjukt bröd" 0.8)
+  , ( "Gurka", Food "Gurka" 0.7)
+  , ( "Kål", Food "Kål" 0.4)
+  , ( "Broccoli", Food "Broccoli" 0.4)
+  ,  ( "Potatis", Food "Potatis" 0.3)
+  ,  ( "Isbergssallad", Food "Isbergssallad" 0.3)
+  ,  ( "Morötter", Food "Morötter" 0.2)
+  ]
 
 foods =
   [ { name = "Nötkött", co2equivalents = 31.0 }
@@ -27,3 +53,11 @@ foods =
   ,  { name = "Isbergssallad", co2equivalents = 0.3 }
   ,  { name = "Morötter", co2equivalents = 0.2 }
   ]
+
+getC02equivalent : String -> Float -> Float
+getC02equivalent foodName amount =
+  amount * (Maybe.withDefault 0 (Maybe.map getC02 (Dict.get foodName foodsDict)))
+
+getC02 : Food -> Float
+getC02 food =
+  food.co2equivalents
